7b9ca9d49647f04534394414b52163774cdb5d0b
ENvironmentSet <sbshw1@gmail.com>
2022-03-25 21:18:26 +0900

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { navigate } from 'gatsby';
 import styled from 'styled-components';
 
-import { Modal, Tabs, Tab } from '@react95/core';
+import { Modal, Frame } from '@react95/core';
 import { Msnstart100 } from '@react95/icons';
 import Layout from '../components/Layout';
+import { useMediaQuery } from 'react-responsive';
 
 const AboutModal = styled(Modal)`
   position: relative;
@@ -20,7 +21,17 @@ const AboutModal = styled(Modal)`
   }
 `;
 
+const ContentBox = styled.div`
+  padding-top: 1em;
+  padding-left: 1em;
+  word-break: keep-all;
+  zoom: 80%;
+`;
+
 export default function About() {
+  const hasNarrowScreen = useMediaQuery({ query: '(max-width: 1112px)' });
+
+
   return (
     <Layout>
       <AboutModal
@@ -28,12 +39,22 @@ export default function About() {
         title='About.exe'
         closeModal={() => navigate(-1)}
       >
-        <Tabs>
-          <Tab title='programming'>
-          </Tab>
-          <Tab title='poem'>작성중</Tab>
-          <Tab title='fashion'>작성중</Tab>
-        </Tabs>
+        <Frame height={hasNarrowScreen ? '67vh' : '62vh'} padding={4}>
+          <Frame css='overflow-y: auto' height='100%' boxShadow='in' bg='white'>
+            <ContentBox>
+              <p>세상을 방랑하는 철학가입니다. 프로그래밍과 작문, 그리고 여행을 즐기며 바삐 돌아가는 세상 속에서 느긋하게 살고자 합니다.</p>
+
+              <hr />
+
+              <h4>외부 링크</h4>
+              <ul>
+                <li>기술 블로그 : <a href='https://overcurried.com'>overcurried</a></li>
+                <li>인스타그램 : <a href='https://www.instagram.com/st4rdus7__/'>@st4rdus7__</a></li>
+                <li>메일 : <a href='mailto:sbshw1@gmail.com'>sbshw1@gmail.com</a></li>
+              </ul>
+            </ContentBox>
+          </Frame>
+        </Frame>
       </AboutModal>
     </Layout>
   )
